c094acdb651653b01b7aceabe0aecb077a853e19
Ashwin-3cS <flashashwin2004@gmail.com>
2025-02-26 20:30:42 +0530

    added a script to just get the current price feed

--- backend/routes/bet.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { placeBet, getBetsByMarket } = require("../controllers/betController");
-
-const router = express.Router();
-
-router.post("/place", placeBet);
-router.get("/:marketId", getBetsByMarket);
-
-module.exports = router;
